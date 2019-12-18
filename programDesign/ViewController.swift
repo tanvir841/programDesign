2218312a17256ad1a720ef22dfda61be2f328601
Abdullah Al Numan <ab>
2019-12-17 22:05:19 -0800

    add comment

--- programDesign/ViewController.swift
+++ programDesign/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//up
+    @IBOutlet weak var bgimage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        let logoImage =  UIImageView(image: #imageLiteral(resourceName: "casino"))
+        view.addSubview(logoImage)
+        //logoImage.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        logoImage.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        logoImage.heightAnchor.constraint(equalToConstant: 270).isActive = true
+        
+        //_ = UILabel().text
+        
         // Do any additional setup after loading the view.
     }
 
